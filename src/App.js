031f1fa9285349c9c924e7f19fe87f316805be88
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2022-02-07 15:13:22 +0200

    start add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.less';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchWeatherData } from './store/weatherSlice';
