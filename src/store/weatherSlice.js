b9a910b0180dc0c0fbc581b00145b2449830fff5
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2022-02-08 09:25:09 +0200

    change http protocol to https

--- src/store/weatherSlice.js
+++ src/store/weatherSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchWeatherData = createAsyncThunk('weather/fetchWeatherData',
     async function (_, { rejectWithValue }) {
         try {
-            const response = await fetch('http://api.weatherapi.com/v1/current.json?key=0730661e7d8f491886875906220602&q=London&aqi=no');
+            const response = await fetch('https://api.weatherapi.com/v1/current.json?key=0730661e7d8f491886875906220602&q=London&aqi=no');
 
             if (!response.ok) {
                 throw new Error('Server Error!');
