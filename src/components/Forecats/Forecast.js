ef0fde70753ef324766bedfa6cafc7b0db68f281
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2022-02-10 13:15:10 +0200

    Add scroll to forecast

--- src/components/Forecats/Forecast.js
+++ src/components/Forecats/Forecast.js
@@ -4,7 +4,7 @@ import { filterDate } from "./helper";
 export default function Forecast() {
     const { forecastday } = useSelector(state => state.forecast.forecastData.forecast);
     return (
-        <div>
+        <div className="forecast">
             {forecastday.map(day => day.hour.filter(time => filterDate(
                 time.time.slice(10, time.time.length - 3),
                 time.time.slice(8, 10),
